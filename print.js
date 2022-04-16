688106de27e72aafe395acf85fbb1ed442557306
Zeprox <68190185+Zeprox@users.noreply.github.com>
2022-04-16 23:00:31 +0700

    Update print.js

--- print.js
+++ print.js
@@ -1,10 +1,11 @@
+window.addEventListener('load', function() {
 function printm(ClassName) {
-        var printContents = document.getElementsByClassName(ClassName)[0].innerHTML;
-        var originalContents = document.body.innerHTML;
-        document.body.innerHTML = printContents;
-        window.print();
-        document.body.innerHTML = originalContents;
-        document.getElementsByClassName('prints')[0].children[0].addEventListener("click", function(){printm('main-reading-area')});
-    };
-document.getElementsByClassName('prints')[0].children[0].addEventListener("click", function(){printm('main-reading-area')});console.log('insert it')
-
+                var printContents = document.getElementsByClassName(ClassName)[0].innerHTML;
+                var originalContents = document.body.innerHTML;
+                document.body.innerHTML = printContents;
+                window.print();
+                document.body.innerHTML = originalContents;
+                document.getElementsByClassName('prints')[0].children[0].addEventListener("click", function(){printm('main-reading-area')});
+        };
+        document.getElementsByClassName('prints')[0].children[0].addEventListener("click", function(){printm('main-reading-area')})
+}
