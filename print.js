4f3a48abdcb920e307ca2f17ec9d5fe0466b082c
Zeprox <68190185+Zeprox@users.noreply.github.com>
2022-04-17 10:22:47 +0700

    Update print.js

--- print.js
+++ print.js
@@ -1,12 +1,9 @@
-window.addEventListener('load', function() {
-  function printm(ClassName) {
-    var printContents = document.getElementsByClassName(ClassName)[0].innerHTML;
-    var originalContents = document.body.innerHTML;
-    document.body.innerHTML = printContents;
-    window.print();
-    document.body.innerHTML = originalContents;
-    document.getElementsByClassName('prints')[0].children[0].addEventListener("click", function(){printm('main-reading-area')});
-    };
-document.getElementsByClassName('prints')[0].children[0].addEventListener("click", function(){printm('main-reading-area')})
-}, false);
-document.getElementsByClassName('prints')[0].children[0].onclick = function(){console.log('test')}
+function printm(ClassName) {
+  var printContents = document.getElementsByClassName(ClassName)[0].innerHTML;
+  var originalContents = document.body.innerHTML;
+  document.body.innerHTML = printContents;
+  window.print();
+  document.body.innerHTML = originalContents;
+  document.getElementsByClassName('prints')[0].children[0].addEventListener("click", function(){printm('main-reading-area')});
+  };
+document.getElementsByClassName('prints')[0].children[0].onclick = function(){printm('main-reading-area')}
