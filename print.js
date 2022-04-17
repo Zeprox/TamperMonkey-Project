7ade54abc473e768273d1a7ce6265bc9cfa96c26
Zeprox <68190185+Zeprox@users.noreply.github.com>
2022-04-17 16:57:49 +0700

    Update print.js

--- print.js
+++ print.js
@@ -75,5 +75,5 @@ if(nurl[0] == 'chapter'){
   document.getElementsByClassName('prints')[0].children[0].onclick = function(){printm('main-reading-area')}
 }
 if(nurl[0] == 'komik'){
-  document.getElementsByClassName('design-menu')[0].onclick = function(){formdata()
+  document.getElementsByClassName('design-menu')[0].onclick = function(){formdata()}
 }
